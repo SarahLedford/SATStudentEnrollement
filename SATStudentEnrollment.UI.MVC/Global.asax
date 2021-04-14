2604ce9d2ee7add3ecd3e91bb29abc6697e5eb74
SarahLedford <Sarah_Ledford@outlook.com>
2021-04-14 11:09:23 -0500

    inital commit

---
+++ SATStudentEnrollment.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SATStudentEnrollment.UI.MVC.MvcApplication" Language="C#" %>
---
+++ SATStudentEnrollment.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SATStudentEnrollment.UI.MVC.Startup))]
+namespace SATStudentEnrollment.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
